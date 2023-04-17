fdf657b167a7c28919b75d68b3c4f050bdcf43b5
CookiessManis <hafiz.purnama2002@gmail.com>
2023-04-17 10:42:15 +0700

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ADE4DB",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+      },
+    },
+  },
+  plugins: [],
+};
+
